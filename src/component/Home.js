49722e4c8563bb215a367543a3504e9c01c78cad
tkrkr55 <tkrkr55@naver.com>
2023-02-10 19:12:43 +0900

    freshman first commit

--- src/component/Home.js
+++ src/component/Home.js
@@ -10,8 +10,11 @@ const Home = () => {
   let [제목,제목변경] = useState(0)
   const [modal,setModal]=useState(false)
   let [입력값,입력값변경] = useState('');
-  let [date,setDate] =useState([19,18,17])
+  let [date,setDate] =useState(0)
   let [modaladd,setmodaladd]=useState('')
+  let[상세내용,상세내용변경] =useState(['내용'])
+  let[render,setRender] =useState('')
+  
 
   const clickchange = ()=>{
     let copy = [...글제목]
@@ -24,11 +27,11 @@ const Home = () => {
       clickchange()
     }
   }
-
+ 
   return (
     <>
  
-       <input placeholder="오늘 하루는 어땟어?" onKeyPress={onkeypress} onChange={(e)=>{
+       <input className='homeinput' placeholder="오늘 하루는 어땟어?" onKeyPress={onkeypress} onChange={(e)=>{
         입력값변경(e.target.value)
       }} type="text" ></input>
  
@@ -53,11 +56,12 @@ const Home = () => {
        }}>
       🤍</span>{좋아요[i]}</h4> 
       </div>
+      
       <div className='day'>
       <p><Year  id="Tear" format={"YYYY"} ticking={false}timezone={"KR/Pacific"}></Year>년</p>
       <p><Month format={"MMM"} ticking={false} timezone={"KR/Pacific"}
       ></Month>월</p>
-      <p> {date[i]}일</p>
+      <p> 19일</p>
       </div>
       
       </div>
@@ -71,7 +75,7 @@ const Home = () => {
    
       
       {
-        modal == true ? <Modal setmodaladd={setmodaladd} modaladd={modaladd} 입력값변경={입력값변경} setModal={setModal} modal={modal} 글제목={글제목} 글제목변경={글제목변경} 제목={제목}/> : null
+        modal == true ? < Modal render={render} setRender={setRender} 상세내용={상세내용} 상세내용변경={상세내용변경} setmodaladd={setmodaladd} modaladd={modaladd} 입력값변경={입력값변경} setModal={setModal} modal={modal} 글제목={글제목} 글제목변경={글제목변경} 제목={제목}/> : null
       }
       
     </>
@@ -80,33 +84,44 @@ const Home = () => {
 }
 
 
-function Modal({setmodaladd,modaladd,입력값변경,modal,setModal,글제목,글제목변경,제목}){
+function Modal({상세내용,상세내용변경,setmodaladd,modaladd,입력값변경,modal,setModal,글제목,글제목변경,제목}){
 
-  
+  const onkeypress=(e)=>{
+    if(e.key == "Enter"){
+      return onclick()
+    }
+  }
+ 
+
+  const onclick=()=>{
+    let copy = [...modaladd]
+    copy.unshift(상세내용)
+    상세내용변경(copy)
+    setmodaladd('')
+  }
   return(
     <div className='modal'>
-      <button onClick={()=>{
+      <button className='엑스' onClick={()=>{
         setModal(false)
       }}>X</button>
       <div className='modaltext'>
       <h3 className='modal_list'>제목 : {글제목[제목]}</h3>
       <p>
-        {modaladd}</p>
-      <input className='modalinput'
-    
+        {상세내용}</p>
+      <input value={modaladd} className='modalinput'
+    onKeyPress={onkeypress}
       onChange={(e)=>{
        setmodaladd(e.target.value)
       }}
-      placeholder="추가내용을 써주세요" ></input>
+      placeholder="추가내용을 써주세요✨" ></input>
       
-    
       </div>
       <button 
         className='modlabtn'
       onClick={()=>{
-        let copy= [...modaladd]
+        let copy= [...상세내용]
         copy.splice(0,1000000000)
-        setmodaladd(copy)
+        상세내용변경(copy)
       }}>삭제하기</button>
       <div  className='modal_layer'></div>
     </div>
